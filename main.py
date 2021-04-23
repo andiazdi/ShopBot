7c40cecdaf6c131a5ec975b0eeda203704791984
andiazdi <68683713+andiazdi@users.noreply.github.com>
2021-04-23 15:18:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 Almaz Andukov <>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Eclipse Distribution License v1.0 which accompany this distribution.
+#
+# The Eclipse Public License is available at
+#    http://www.eclipse.org/legal/epl-v10.html
+# and the Eclipse Distribution License is available at
+#   http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# Contributors:
+#    Almaz Andukov
+
 import telebot
 from functions import *
 import logging
