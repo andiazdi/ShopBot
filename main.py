970a640711650247cb731a2d8e243dd10d55dba2
andiazdi <68683713+andiazdi@users.noreply.github.com>
2021-04-23 15:19:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2020 Almaz Andukov <>
+# Copyright (c) 202 Almaz Andukov <>
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
